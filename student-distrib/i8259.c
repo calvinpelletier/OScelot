e5b047b80d772082b6b90ef66d47ac89caa91e80
Calvin Pelletier <calvin.pelletier@gmail.com>
2016-03-10 21:10:36 -0600

    added enable and disable irq functions.

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -20,17 +20,17 @@ void send_eoi(unsigned int irq_num);
 // GLOBAL VARIABLES
 /* Interrupt masks to determine which interrupts
  * are enabled and disabled */
-unsigned char master_mask; /* IRQs 0-7 */
-unsigned char slave_mask; /* IRQs 8-15 */
+unsigned char master_mask; /* IRQs 7-0 */
+unsigned char slave_mask; /* IRQs 15-8 */
 
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
     // mask all interrupts
-    outb(MASTER_DATA, 0x00);
-    outb(SLAVE_DATA, 0x00);
-    master_mask = 0;
-    slave_mask = 0;
+    outb(MASTER_DATA, 0xFF);
+    outb(SLAVE_DATA, 0xFF);
+    master_mask = 0xFF;
+    slave_mask = 0xFF;
 
     // TODO: should we wait at all between commands?
     // initialize master
@@ -51,16 +51,48 @@ void i8259_init(void) {
             printf("ERROR: master and/or slave interrupts are not all masked after PIC initialization.")
         }
     }
+
+    //TODO: initialize devices
 }
 
 
 /* Enable (unmask) the specified IRQ */
 void enable_irq(unsigned int irq_num) {
+    // check if irq is on master or slave
+    if (irq_num < 8) { // master
+        if (master_mask & (1 << irq_num)) { // not already enabled
+            outb(MASTER_DATA, inb(MASTER_DATA) & ~(1 << irq_num));
+            master_mask &= ~(1 << irq_num);
+        }
+    } else if (irq_num < 16) { // slave
+        irq_num -= 8;
+        if (slave_mask & (1 << irq_num)) { // not already enabled
+            outb(SLAVE_DATA, inb(SLAVE_DATA) & ~(1 << irq_num));
+            slave_mask &= ~(1 << irq_num);
+        }
+    } else { // error
+        printf("ERROR: invalid irq_num in enable_irq.");
+    }
 }
 
 
 /* Disable (mask) the specified IRQ */
 void disable_irq(unsigned int irq_num) {
+    // check if irq is on master or slave
+    if (irq_num < 8) { // master
+        if (!(master_mask & (1 << irq_num))) { // not already disabled
+            outb(MASTER_DATA, inb(MASTER_DATA) | (1 << irq_num));
+            master_mask |= ~(1 << irq_num);
+        }
+    } else if (irq_num < 16) { // slave
+        irq_num -= 8;
+        if (!(slave_mask & (1 << irq_num))) { // not already disabled
+            outb(SLAVE_DATA, inb(SLAVE_DATA) | (1 << irq_num));
+            slave_mask |= (1 << irq_num);
+        }
+    } else { // error
+        printf("ERROR: invalid irq_num in disable_irq.");
+    }
 }
 
 
--- student-distrib/i8259.h
+++ student-distrib/i8259.h
@@ -34,10 +34,10 @@
 /* Initialize both PICs */
 void i8259_init(void);
 /* Enable (unmask) the specified IRQ */
-void enable_irq(uint32_t irq_num);
+void enable_irq(unsigned int irq_num);
 /* Disable (mask) the specified IRQ */
-void disable_irq(uint32_t irq_num);
+void disable_irq(unsigned int irq_num);
 /* Send end-of-interrupt signal for the specified IRQ */
-void send_eoi(uint32_t irq_num);
+void send_eoi(unsigned int irq_num);
 
 #endif /* _I8259_H */
