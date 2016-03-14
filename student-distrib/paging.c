a12ec875a9f7aee593bb2499084d05331545de79
Calvin Pelletier <calvin.pelletier@gmail.com>
2016-03-13 20:24:15 -0500

    started initialization function

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -2,3 +2,21 @@
 
 #include "paging.h"
 #include "lib.h"
+
+
+// FUNCTION DECLARATIONS
+void paging_init(void);
+
+
+// GLOBAL VARIABLES
+static unsigned int pageDir[1024] __attribute__((aligned(4096)));
+
+
+// GLOBAL FUNCTIONS
+void paging_init(void) {
+    // initialize pageDir
+    int i;
+    for (i = 0; i < 1024; i++) {
+        pageDir[i] = 0x00000002; // this sets the flags to kernel-only, write-enabled, and not-present
+    }
+}
