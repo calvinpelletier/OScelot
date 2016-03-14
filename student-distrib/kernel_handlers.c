72ce893e05415645b870d9d342d0c7cc87aaecc3
Nachiket Joshi <nachiketjoshi6@gmail.com>
2016-03-14 01:45:14 -0500

    THANK YOU BASED JOHN for fixing the GODDAMN IDT NMI/DB0 error thing

--- student-distrib/kernel_handlers.c
+++ student-distrib/kernel_handlers.c
@@ -3,160 +3,160 @@
 
 void divideByZero()
 {
-	clear();
-    printf("0x00");
+	;
+    printf("\n0x00\n");
     printf("OScelot won't let you divide by zero");
 	while(1);
 }
 
 void debug()
 {
-	clear();
-    printf("0x01");
+	;
+    printf("\n0x01\n");
     printf("OScelot won't let you debug");
 	while(1);
 }
 
 void nonMaskableInterrupts()
 {
-	clear();
-    printf("0x02");
+	;
+    printf("\n0x02\n");
     printf("OScelot asks about NMIs");
 	while(1);
 }
 
 void breakpoint()
 {
-	clear();
-    printf("0x03");
+	;
+    printf("\n0x03\n");
     printf("OScelot hits a breakpoint");
 	while(1);
 }
 
 void overflow()
 {
-	clear();
-    printf("0x04");
+	;
+    printf("\n0x04\n");
     printf("OScelot's cup has overflowed");
 	while(1);
 }
 
 void bounds()
 {
-	clear();
-    printf("0x05");
+	;
+    printf("\n0x05\n");
     printf("You are intruding on OScelot's bounds");
 	while(1);
 }
 
 void invalidOpCode()
 {
-	clear();
-    printf("0x06");
+	;
+    printf("\n0x06\n");
     printf("OScelot doesn't understand your speech");
 	while(1);
 }
 
 void coprocessorNotAvailable()
 {
-	clear();
-    printf("0x07");
+	;
+    printf("\n0x07\n");
     printf("OScelot's pardner ain't here yet");
 	while(1);
 }
 
 void doubleFault()
 {
-	clear();
-    printf("0x08");
+	;
+    printf("\n0x08\n");
     printf("OScelot hit a wall ... again");
 	while(1);
 }
 
 void coprocessorSegmentOverrun()
 {
-	clear();
-    printf("0x09");
+	;
+    printf("\n0x09\n");
     printf("OScelot's pardner's segment has been overrun");
 	while(1);
 }
 
 void invalidTaskStateSegment()
 {
-	clear();
-    printf("0x0A");
+	;
+    printf("\n0x0A\n");
     printf("OScelot's TSS failed. You incompetent programmer");
 	while(1);
 }
 
 void segmentNotPresent()
 {
-	clear();
-    printf("0x0B");
+	;
+    printf("\n0x0B\n");
     printf("OScelot doesn't know what segment you are talking about");
 	while(1);
 }
 
 void stackFault()
 {
-	clear();
-    printf("0x0C");
+	;
+    printf("\n0x0C\n");
     printf("OScelot suggests you check the stack fault thingamajig");
 	while(1);
 }
 
 void generalProtectionFault()
 {
-	clear();
-    printf("0x0D");
+	;
+    printf("\n0x0D\n");
     printf("OScelot can't protect you any more");
 	while(1);
 }
 
 void pageFault()
 {
-	clear();
-    printf("0x0E");
+	;
+    printf("\n0x0E\n");
     printf("OScelot's book has a page fault. He can't read his book now. Happy?");
 	while(1);
 }
 
 void reserved()
 {
-	clear();
-    printf("0x0F");
+	;
+    printf("\n0x0F\n");
     printf("OScelot won't let you do that");
 	while(1);
 }
 
 void mathFault()
 {
-	clear();
-    printf("0x10");
+	;
+    printf("\n0x10\n");
     printf("OScelot can't do the math");
 	while(1);
 }
 
 void alignmentCheck()
 {
-	clear();
-    printf("0x11");
+	;
+    printf("\n0x11\n");
     printf("OScelot asks you to check your alignment");
 	while(1);
 }
 
 void machineCheck()
 {
-	clear();
-    printf("0x12");
+	;
+    printf("\n0x12\n");
     printf("OScelot needs you to check your machine and cry");
 	while(1);
 }
 
 void simdFloatingPointException()
 {
-	clear();
-    printf("0x13");
+	;
+    printf("\n0x13\n");
     printf("OScelot can't handle floats right now");
 	while(1);
 }
