229397ceac422eec57beddf196e13f849fe2adec
Calvin Pelletier <calvin.pelletier@gmail.com>
2016-03-25 18:20:30 -0500

    added a bunch of useful structs

--- student-distrib/filesys.c
+++ student-distrib/filesys.c
@@ -3,13 +3,57 @@
 #include "filesys.h"
 
 
+// STRUCTS
+struct dentry_t {
+    unsigned char name[32];
+    int type;
+    unsigned int inode;
+    unsigned char reserved[24];
+};
+
+struct bootblock_t {
+    unsigned int n_dentries;
+    unsigned int n_inodes;
+    unsigned int n_datablocks;
+    unsigned char reserved[52];
+    dentry_t dentries[63];
+};
+
+struct inode_t {
+    unsigned int length; // in bytes
+    unsigned int datablocks[1023];
+};
+
+struct fileops_t {
+    int (*open)(unsigned char*);
+    int (*read)(int, unsigned char*, int);
+    int (*write)(int, unsigned char*, int);
+    void (*close)(int);
+};
+
+struct fileflags_t {
+    unsigned int in_use : 1;
+    unsigned int read_only : 1;
+    unsigned int write_only : 1;
+}
+
+struct file_t {
+    fileops_t jumptable;
+    inode_t inode;
+    unsigned int position;
+    fileflags_t flags;
+}
+
+
+// GLOBAL VARIABLES
+
+
+
 // FUNCTION DECLARATIONS
 int init_filesys(void);
-int read_dentry_by_name(const unsigned char* fname, dentry_t* dentry);
-int read_dentry_by_index(unsigned int index, dentry_t* dentry);
-int read_data(unsigned int inode, unsigned int offset, unsigned char* buf, unsigned int length);
+
 
 // GLOBAL FUNCTIONS
 int init_filesys(void) {
-    
+
 }
--- student-distrib/filesys.h
+++ student-distrib/filesys.h
@@ -5,13 +5,6 @@
 
 #include "types.h"
 
-struct dentry_t {
-    unsigned char name[32];
-    int type;
-    unsigned int n_inodes;
-    unsigned char reserved[24];
-}
-
 
 // GLOBAL FUNCTIONS
 
