3bcff4c8179bd79960653e39a7b28a684388a488
Nachiket Joshi <nachiketjoshi6@gmail.com>
2016-03-28 15:24:30 -0500

    Added implementations for file ops

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -14,11 +14,12 @@
 static int count = 0;
 #endif
 
-
 // FUNCTION DECLARATIONS
 void rtc_init(void);
 void rtcHandler(void);
 
+// Flag for rtc_read
+volatile char rtc_interrupt_flag;
 
 // GLOBAL FUNCTIONS
 /*
@@ -57,63 +58,100 @@ void rtcHandler(void) {
     }
 
     send_eoi(RTC_IRQ_NUM);
+    rtc_interrupt_flag = 1;
 }
 
 /*
- * open
+ * rtc_open
  * DESCRIPTION:
  * INPUTS:
  * OUTPUTS:
  * RETURNS:
  * NOTES:
  */
-int32_t void open(const uint8_t *filename)
+int32_t rtc_open(const uint8_t *filename)
 {
+    int open_hertz = 2;
+    int *pass;
+    pass = &open_hertz;
 
+    rtc_write(0, pass, 4);
 }
 
 /*
- * read
+ * rtc_read
  * DESCRIPTION:
  * INPUTS:
  * OUTPUTS:
  * RETURNS:
  * NOTES:
  */
-int32_t void read(int32_t fd, void *buf, int32_t nbytes)
+int32_t rtc_read(int32_t fd, void *buf, int32_t nbytes)
 {
-
-}
-
-/*
- * write
- * DESCRIPTION: Changes RTC interrupt frequency by a specified rate
- * INPUTS:
- * OUTPUTS:
- * RETURNS:
- * NOTES:
- */
-int32_t write(int32_t fd, const void *buf, int32_t nbytes)
-{
-    char rate = (char) buf;
-    rate &= 0x0F;
-    outb(0x70, 0x8A);
-    char prev=inb(0x7a);
-    outb(0x70, 0x8A);
-    outb(0x71, (prev & 0xF0) | rate);
-    printf("rate changeed");
+    while(rtc_interrupt_flag != 1) {
+        // wait for rtc_interrupt_flag to be set to one.
+    }
     return 0;
 }
 
 /*
- * close
+ * rtc_write
+ * DESCRIPTION: Changes RTC interrupt frequency by a specified rate
+ * INPUTS: fd     - not used
+ *         buf    -
+ *         nbytes -
+ * OUTPUTS: none
+ * RETURNS: The number of bytes written.
+ * NOTES:
+ */
+int32_t rtc_write(int32_t fd, const void *buf, int32_t nbytes)
+{
+    int i;
+    int num_bytes = 0;
+
+    if (buf != NULL) {
+        int rate = *(int*)buf;
+        int check = *(int*)buf;
+        int flag = 1;
+
+        while (check != 1) {
+            if (check%2 != 0)
+                flag = 0;
+            check /= 2;
+        }
+
+        /*
+         * If the rate is not a power of two
+         * or the rate is greater than 1024,
+         * fail gracefully
+         */
+        if (!flag || rate > 1024)
+            return -1;
+
+        cli();
+        outb(RTC_ADDR, 0x8A);
+        num_bytes++;
+        char prev = inb(RTC_DATA);
+        outb(RTC_ADDR, 0x8A);
+        num_bytes++;
+        outb(RTC_DATA, rate);
+        num_bytes++;
+        sti();
+    } else {
+        return -1;
+    }
+    return num_bytes;
+}
+
+/*
+ * rtc_close
  * DESCRIPTION:
  * INPUTS:
  * OUTPUTS:
  * RETURNS:
  * NOTES:
  */
-int32_t void close(int32_t fd)
+int32_t rtc_close(int32_t fd)
 {
-
+    return -1;
 }
