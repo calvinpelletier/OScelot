a6ff7560c5c5c55196958e83a26cfd8930c79862
Calvin Pelletier <calvin.pelletier@gmail.com>
2016-04-10 20:43:57 -0500

    fixed exe check

--- student-distrib/syscalls.c
+++ student-distrib/syscalls.c
@@ -6,6 +6,7 @@
 
 // CONSTANTS
 #define MAX_FNAME_LEN 32;
+unsigned char MAGIC_EXE_NUMS[4] = {0x7f, 0x45, 0x4c, 0x46};
 
 
 // FUNCTION DECLARATIONS
@@ -38,25 +39,22 @@ int execute (unsigned char* command) {
         exename[i] = command[i];
     }
 
-    // exe check
+    // fetch file
     unsigned char buf[MAX_FNAME_LEN];
     int fd, count;
-    if ((fd = fs_open((char*)".")) == -1) {
+    if ((fd = fs_open(exename) == -1) {
         return -1;
     }
-    int fail = 1;
-    while ((count = fs_read(fd, buf, MAX_FNAME_LEN))) {
-        if (count == -1) {
-            return -1;
-        }
-        if (!strncmp(exename, buf, MAX_FNAME_LEN)) {
-            fail = 0;
-            break;
-        }
-    }
-    if (fail) {
+
+    // exe check
+    unsigned char first_bytes[4];
+    if (fs_read(fd, first_bytes, 4)) {
         return -1;
     }
+    if (strncmp(MAGIC_EXE_NUMS, first_bytes, 4)) {
+        return -1;
+    }
+
 
     // set up paging
     // file loader
