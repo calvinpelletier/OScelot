bd95bde1767e6947222618e7f1e3829dc64e2f87
jhryan3 <jhryan3@illinois.edu>
2016-04-10 22:43:12 -0500

    Some changes to filesys/syscalls
    Adding pcb, working on OCRW, taking fd stuff from filesys and putting in
    syscalls.

--- student-distrib/syscalls.h
+++ student-distrib/syscalls.h
@@ -1,6 +1,28 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+#include "filesys.h"
+#define MAX_FD 8
+
+/*
+ *	Process Control Block used to describe each process. Contains data
+ *  pertinent to the current process. WORK IN PROGRESS
+ *	
+ * 	Fields:
+ *	fd_array: Array of file descriptors, contains info on each file opened by process. 
+ * 			  First two files are stdin and stdout, up to 6 more files can be opened after that.
+ *	PID: Process number/ID of the current running process.
+ * 	PPID: Parent process number/ID of the current running process.
+ *
+ */
+
+typedef struct {
+	file_t fd_array[MAX_FD];
+	unsigned char PID; 
+	unsigned char PPID;
+} pcb_t;
+
+// System Calls
 extern int halt (unsigned char status);
 extern int execute (unsigned char* command);
 extern int read (int fd, void* buf, int nbytes);
@@ -11,5 +33,7 @@ extern int getargs (unsigned char* buf, int nbytes);
 extern int vidmap (unsigned char** screenstart);
 extern int set_handler (int signum, void* handler_address);
 extern int sigreturn (void);
+
+
 
 #endif
